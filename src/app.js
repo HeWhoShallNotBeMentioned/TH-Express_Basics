435289c9fc5aa6eee8aa54dcc51a62d54703bfff
Chris Underwood <cunderwoodmn@gmail.com>
2017-04-18 14:56:35 -0700

    add route for /blog and required posts.json so we have access to it

--- src/app.js
+++ src/app.js
@@ -1,11 +1,16 @@
 'use strict';
 
 const express = require('express');
+const posts = require('./mock/posts.json');
 
 let app = express();
 
 app.get('/', (req, res) => {
   res.send('<h1>Node.js is interesting.</h1>');
+});
+
+app.get('/blog', (req, res) => {
+  res.send(posts);
 });
 
 app.listen(3000, () => {
